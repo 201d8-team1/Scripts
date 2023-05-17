42d01a84bc46469b1f75fc8ce7feaaf35dd85acf
jaehwanjung23 <jaehwanjung23@gmail.com>
2023-05-17 19:31:28 +0000

    Enable PSRemote script

---
+++ enable_psremote.ps1
@@ -0,0 +1,3 @@
+Enable-PSremoting -Force -SkipNetworkProfileCheck
+#Only allows connections from within the LAN.
+#For connections from outside of the LAN, refer to the Enable PSRemote & RDP Documentation
