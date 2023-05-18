36dd447b64a5d8d3dfe42121c10f5204dd415f2f
David Prutch <77696749+PrutchD@users.noreply.github.com>
2023-05-17 18:40:22 -0700

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -5,7 +5,7 @@
 @REM                               Also adds a log of the backup to the backup drive files
 @REM                               Requires a task to be scheduled in Windows Task Scheduler to perform operation at EOD.
 @REM                               For each new user created change admin to username
-@REM                               
+@REM                               Verify path for each segment is correct order is copy from location path copy to location path log path should match copy to location path
 
 @REM Main
 robocopy "C:\Users\Admin\Documents" "C:\Users\Admin\OneDrive\Documents" /E /log:"C:\Users\Admin\OneDrive\Documents\backup_log.txt"
