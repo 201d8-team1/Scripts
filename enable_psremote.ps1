f042b276ad504cc7dfdfa3e7b630eabe822656f6
jaehwanjung23 <58351858+jaehwanjung23@users.noreply.github.com>
2023-05-19 14:18:22 -0400

    Update enable_psremote.ps1

--- enable_psremote.ps1
+++ enable_psremote.ps1
@@ -1,3 +1,6 @@
+#### Script Name:                  Enable PSRemote
+#### Author:                       Jason Jung
+#### Date of latest revision:      05/16/2023
+#### Purpose:                      Enable PSRemote on a Windows 10 Machine for local connections.
+
 Enable-PSremoting -Force -SkipNetworkProfileCheck
-#Only allows connections from within the LAN.
-#For connections from outside of the LAN, refer to the Enable PSRemote & RDP Documentation
